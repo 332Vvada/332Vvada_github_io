5f02054755ca1d0124076a28ea549c401de7feca
332Vvada <shpakjunior@gmail.com>
2024-11-22 20:22:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,52 @@
-const slider = document.querySelector(".slider");
-
-const slides = Array.from(slider.children);
-
-const slide_1 = document.querySelector("#slide-1");
-
-const slide_2 = document.querySelector("#slide-2");
-
-const slide_3 = document.querySelector("#slide-3");
-
 const prevButton = document.querySelector(".button-prev");
 
 const nextButton = document.querySelector(".button-next");
 
-let images = [
-  "media/dentistry-1.png",
-  "media/dentistry-2.png",
-  "media/dentistry-3.png",
-  "media/image.png",
-];
+const sliderContainer = document.getElementById("sliderContainer");
+let slideWidth = document.querySelector(".slide").offsetWidth;
+let currentTranslate = 0;
+let numSlides = document.querySelectorAll(".slide").length;
 
-let previousSlide = 0;
-let currentSlide = 1;
-let followingSlide = 2;
+const slides = Array.from(sliderContainer.children);
 
-function showSlide(prevIndex, index, nextIndex) {
-  slide_1.src = images[prevIndex];
-  slide_2.src = images[index];
-  slide_3.src = images[nextIndex];
-  previousSlide = prevIndex;
-  currentSlide = index;
-  followingSlide = nextIndex;
-  console.log(
-    `active slides: ${previousSlide} ${currentSlide} ${followingSlide}`
-  );
+let currentSlide;
+
+let SliderInterval;
+const IntervalTimer = 4000;
+
+function showSlide() {
+  currentSlide = Math.floor(currentTranslate / -440) + 1;
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[currentSlide].classList.add("active");
+  console.log(`текущая ширина: ${currentTranslate}`);
+  console.log(`текущий слайд: ${currentSlide}`);
 }
 
 function nextSlide() {
-  previousSlide = currentSlide;
-  currentSlide = followingSlide;
-  followingSlide++;
-  if (followingSlide >= images.length) {
-    followingSlide = 0;
+  clearInterval(SliderInterval);
+  currentTranslate -= slideWidth;
+  if (currentTranslate < -slideWidth * (numSlides - 2)) {
+    currentTranslate = 440;
   }
-  showSlide(previousSlide, currentSlide, followingSlide);
+  sliderContainer.style.transform = `translateX(${currentTranslate}px)`;
+  showSlide();
+  SliderInterval = setInterval(nextSlide, IntervalTimer);
 }
 
 function prevSlide() {
-  followingSlide = currentSlide;
-  currentSlide = previousSlide;
-  previousSlide--;
-  if (previousSlide < 0) {
-    previousSlide = images.length - 1;
+  clearInterval(SliderInterval);
+  currentTranslate += slideWidth;
+  if (currentTranslate > slideWidth) {
+    currentTranslate = -slideWidth * (numSlides - 2);
   }
-  showSlide(previousSlide, currentSlide, followingSlide);
+  sliderContainer.style.transform = `translateX(${currentTranslate}px)`;
+  showSlide();
+  SliderInterval = setInterval(nextSlide, IntervalTimer);
 }
+
+SliderInterval = setInterval(nextSlide, IntervalTimer);
+showSlide();
 
 prevButton.addEventListener("click", prevSlide);
 
 nextButton.addEventListener("click", nextSlide);
-
-showSlide(previousSlide, currentSlide, followingSlide);
