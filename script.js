5fef06092b7ec47f3a3b42ce8ee60c53599ae17e
332Vvada <shpakjunior@gmail.com>
2024-12-03 19:23:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Form
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // Отменяем стандартное поведение формы
+});
+// Slider
+
 const prevButton = document.querySelector(".button-prev");
 
 const nextButton = document.querySelector(".button-next");
@@ -51,8 +59,8 @@ prevButton.addEventListener("click", prevSlide);
 
 nextButton.addEventListener("click", nextSlide);
 
-window.addEventListener("blur", stopSlider); // Пауза при потере фокуса
-window.addEventListener("focus", startSlider); // Возобновление при получении фокуса
+// window.addEventListener("blur", stopSlider); // Пауза при потере фокуса
+// window.addEventListener("focus", startSlider); // Возобновление при получении фокуса
 
 // function startSlider() {
 //   SliderInterval = setInterval(nextSlide, IntervalTimer);
@@ -61,3 +69,105 @@ window.addEventListener("focus", startSlider); // Возобновление п�
 // function stopSlider() {
 //   SliderInterval && clearInterval(SliderInterval);
 // }
+
+// Modal
+
+const Bg = document.querySelector(".dark-bg");
+const Modal = document.querySelector(".modal");
+const modalCloseButton = document.querySelector("#close-modal-button");
+function openModal() {
+  console.log("open");
+  Bg.style.display = "block";
+  Modal.classList.add("show");
+}
+
+modalCloseButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  console.log("close");
+  Bg.style.display = "none";
+  Modal.classList.remove("show");
+}
+// Validation
+
+// Инпуты и поля предупреждений,а также кнопка и чекбокс
+const validationNameInput = document.querySelector("#inputName");
+const validationNameWarn = document.querySelector(".name-validation");
+const validationPhoneInput = document.querySelector("#inputPhone");
+const validationPhoneWarn = document.querySelector(".phone-validation");
+const validationCheckBox = document.querySelector("#privacy");
+const validationSubmitButton = document.querySelector(".submit-button");
+const validationfullWarn = document.querySelector(".full-validation");
+
+// Статусы
+let ValidName = false;
+let ValidPhone = false;
+
+// Добавление событий проверок
+validationNameInput.addEventListener("blur", checkName);
+
+validationNameInput.addEventListener("focus", inputtingName);
+
+validationPhoneInput.addEventListener("blur", checkPhone);
+
+validationPhoneInput.addEventListener("focus", inputtingPhone);
+
+validationSubmitButton.addEventListener("click", checkValidate);
+
+function checkName() {
+  console.log("checkName");
+  if (validationNameInput.value === "") {
+    if (validationNameWarn.style.display === "block") {
+      validationNameWarn.style.display = "none";
+    } // Если поле пустое, то предупреждение убирается
+    ValidName = false;
+  } else if (/^[А-ЯЁа-яё]+$/.test(validationNameInput.value) != true) {
+    validationNameWarn.style.display = "block";
+    ValidName = false;
+  } // Проверка регулярного выражения
+  else {
+    ValidName = true;
+  } // Прохождение проверки
+}
+
+function inputtingName() {
+  console.log("inputtingName");
+  if (validationNameWarn.style.display === "block") {
+    validationNameWarn.style.display = "none";
+  }
+} // Во время ввода значения предупреждение убирается
+
+function checkPhone() {
+  console.log("checkPhone");
+  if (validationPhoneInput.value === "") {
+    if (validationPhoneWarn.style.display === "block") {
+      validationPhoneWarn.style.display = "none";
+    } // Если поле пустое, то предупреждение убирается
+    ValidPhone = false;
+  }
+  if (/^(89)\d{9}$/.test(validationPhoneInput.value) != true) {
+    validationPhoneWarn.style.display = "block";
+    ValidPhone = false;
+  } // Проверка регулярного выражения
+  else {
+    ValidPhone = true;
+  } // Прохождение проверки
+}
+
+function inputtingPhone() {
+  console.log("inputtingPhone");
+  if (validationPhoneWarn.style.display === "block") {
+    validationPhoneWarn.style.display = "none";
+  }
+} // Во время ввода значения предупреждение убирается
+
+function checkValidate() {
+  if (ValidName === true && ValidPhone === true && validationCheckBox.checked) {
+    //
+    //  Здесь форма должна отправить данные
+    //
+    closeModal();
+  } else {
+    validationfullWarn.style.display = "block";
+  }
+}
